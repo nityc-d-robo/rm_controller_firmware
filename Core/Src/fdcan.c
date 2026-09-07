2b2caee74cde25555d06f88fe2cc270e366c7bf9
Mori-Nagi <nagi@first-line-soft.jp>
2026-09-07 12:11:44 +0900

    ロボストライドを実装

--- Core/Src/fdcan.c
+++ Core/Src/fdcan.c
@@ -160,7 +160,7 @@ void MX_FDCAN2_Init(void)
   hfdcan2.Init.DataTimeSeg1 = 7;
   hfdcan2.Init.DataTimeSeg2 = 2;
   hfdcan2.Init.StdFiltersNbr = 2;
-  hfdcan2.Init.ExtFiltersNbr = 0;
+  hfdcan2.Init.ExtFiltersNbr = 1;
   hfdcan2.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
   if (HAL_FDCAN_Init(&hfdcan2) != HAL_OK)
   {
